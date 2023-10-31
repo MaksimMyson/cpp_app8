ed53e17e81e9c2613801552beb5cd2e32d8c5889
MaksimMyson <maksimmyson33@gmail.com>
2023-10-31 20:03:29 +0200

    ex1

--- Header.h
+++ Header.h
@@ -1,13 +1,10 @@
-#include <iostream>
-using namespace std;
-
 template <typename T>
 struct MinMaxResult {
-    T min_value;
-    T max_value;
-    int min_index;
-    int max_index;
+    T minValue;
+    int minIndex;
+    T maxValue;
+    int maxIndex;
 };
 
 template <typename T>
-MinMaxResult<T> find_min_max(T* arr, int size);
+MinMaxResult<T> findMinMax(const T arr[], int size);
--- main.cpp
+++ main.cpp
@@ -1,32 +1,29 @@
 #include <iostream>
-#include <cstdlib> 
-#include <ctime>  
 #include "header.h"
+#include <cstdlib>
+#include <ctime>
 
 template <typename T>
-MinMaxResult<T> find_min_max(T* arr, int size) {
+MinMaxResult<T> findMinMax(const T arr[], int size) {
     MinMaxResult<T> result;
-    if (size <= 0) {
-        result.min_value = T(); 
-        result.max_value = T(); 
-        result.min_index = -1;
-        result.max_index = -1;
+    if (size == 0) {
+        std::cerr << "Empty array" << std::endl;
+        result.minValue = result.maxValue = T();
+        result.minIndex = result.maxIndex = -1;
+        return result;
     }
-    else {
-        result.min_value = arr[0];
-        result.max_value = arr[0];
-        result.min_index = 0;
-        result.max_index = 0;
 
-        for (int i = 1; i < size; ++i) {
-            if (arr[i] < result.min_value) {
-                result.min_value = arr[i];
-                result.min_index = i;
-            }
-            if (arr[i] > result.max_value) {
-                result.max_value = arr[i];
-                result.max_index = i;
-            }
+    result.minValue = result.maxValue = arr[0];
+    result.minIndex = result.maxIndex = 0;
+
+    for (int i = 1; i < size; ++i) {
+        if (arr[i] < result.minValue) {
+            result.minValue = arr[i];
+            result.minIndex = i;
+        }
+        if (arr[i] > result.maxValue) {
+            result.maxValue = arr[i];
+            result.maxIndex = i;
         }
     }
 
@@ -34,34 +31,22 @@ MinMaxResult<T> find_min_max(T* arr, int size) {
 }
 
 int main() {
-    const int size = 10;  
-    int* arr = new int[size];
+    const int size = 10;
+    int arr[size];
 
-   
     srand(static_cast<unsigned>(time(0)));
-
-    for (int i = 0; i < size; ++i) {
-        arr[i] = rand() % 100; 
-    }
 
     std::cout << "Array elements: ";
     for (int i = 0; i < size; ++i) {
+        arr[i] = rand() % 100;
         std::cout << arr[i] << " ";
     }
     std::cout << std::endl;
 
-    MinMaxResult<int> result = find_min_max(arr, size);
+    MinMaxResult<int> result = findMinMax(arr, size);
 
-    std::cout << "Minimum value: " << result.min_value << " at index " << result.min_index << std::endl;
-    std::cout << "Maximum value: " << result.max_value << " at index " << result.max_index << std::endl;
-
-    // Не забудьте звільнити виділену пам'ять
-    delete[] arr;
+    std::cout << "Minimum value: " << result.minValue << " at index " << result.minIndex << std::endl;
+    std::cout << "Maximum value: " << result.maxValue << " at index " << result.maxIndex << std::endl;
 
     return 0;
 }
-
-
-
-
-
