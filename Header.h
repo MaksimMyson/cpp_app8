5c5b66ef8bd67ebbbb51d9859fa5d7ec42ff288c
MaksimMyson <maksimmyson33@gmail.com>
2023-11-06 17:01:46 +0200

    ex2

--- Header.h
+++ Header.h
@@ -1,2 +1,18 @@
 #include <iostream>
 using namespace std;
+
+void initializeMatrix(int n, int matrix[][3]);
+void initializeMatrix(int n, double matrix[][3]);
+void initializeMatrix(int n, char matrix[][3]);
+
+void printMatrix(int n, int matrix[][3]);
+void printMatrix(int n, double matrix[][3]);
+void printMatrix(int n, char matrix[][3]);
+
+void findMinMaxDiagonal(int n, int matrix[][3], int& minVal, int& maxVal);
+void findMinMaxDiagonal(int n, double matrix[][3], double& minVal, double& maxVal);
+void findMinMaxDiagonal(int n, char matrix[][3], char& minVal, char& maxVal);
+
+void sortRows(int n, int matrix[][3]);
+void sortRows(int n, double matrix[][3]);
+void sortRows(int n, char matrix[][3]);
